2b80db6684ce5193bcaa4decb0d3ac31151cdd66
Patrik Madison <patrik.madison@gmail.com>
2026-04-03 19:27:57 +0200

    actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   base: '/omron-bp-scanner/',
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Omron BP Scanner',
+        short_name: 'OmronScan',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
